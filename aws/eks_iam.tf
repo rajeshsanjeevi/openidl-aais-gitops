41dd0b27a5b2d17a778ab0c1354f8263902b3cb4
rajeshsanjeevi@itpeoplecorp.com <86312527+rajeshsanjeevi@users.noreply.github.com>
2021-08-23 13:52:55 -0400

    Update eks_iam.tf

--- aws/eks_iam.tf
+++ aws/eks_iam.tf
@@ -137,6 +137,7 @@ resource "aws_iam_policy" "eks_admin_group_assume_policy" {
     Name = "${local.std_name}-eks-admin-group-assume",
     Cluster_type = "both"
   })
+  depends_on = [aws_iam_group.eks_admin_group, aws_iam_group_policy_attachment.eks_admin_group_policy_attachment]
 }
 
 
